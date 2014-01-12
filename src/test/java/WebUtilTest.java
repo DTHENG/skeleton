630e3f6bdf7b207699554041c3961904d35f18f0
Daniel Thengvall <fender5289@gmail.com>
2014-01-11 19:26:47 -0800

    added comments

--- src/test/java/WebUtilTest.java
+++ src/test/java/WebUtilTest.java
@@ -1,8 +1,14 @@
 import junit.framework.Assert;
 import org.junit.Test;
 
+/**
+ * @author Daniel Thengvall
+ */
 public class WebUtilTest {
 
+    /**
+     * The simplest of tests
+     */
     @Test
     public void getSimplestTest() {
         Assert.assertTrue(true);
